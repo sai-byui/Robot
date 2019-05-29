8df975e15b8c7dbe88fdb48076f26f8fb970ec5e
axle12693 <axle12693@gmail.com>
2019-05-28 19:25:13 -0600

    make it stop

--- Sunfounder_Smart_Video_Car_Kit_for_RaspberryPi-master/server/test_script.py
+++ Sunfounder_Smart_Video_Car_Kit_for_RaspberryPi-master/server/test_script.py
@@ -1,8 +1,10 @@
 import motor
+import time
 
 motor.setup(busnum=1)
-
-while True:
+t = time.time()
+while time.time < t + 5:
     motor.setSpeed(50)
     motor.motor0('True')
     motor.motor1('True')
+motor.stop()
