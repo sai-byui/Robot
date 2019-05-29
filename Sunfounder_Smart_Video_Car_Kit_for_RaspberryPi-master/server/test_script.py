acc2de2f569b8a719074e83e5de5c4104da01ae6
axle12693 <axle12693@gmail.com>
2019-05-28 19:40:07 -0600

    make it turn

--- Sunfounder_Smart_Video_Car_Kit_for_RaspberryPi-master/server/test_script.py
+++ Sunfounder_Smart_Video_Car_Kit_for_RaspberryPi-master/server/test_script.py
@@ -1,7 +1,12 @@
 import motor
+import car_dir
 import time
 
 motor.setup(busnum=1)
+car_dir.setup()
+car_dir.home()
+car_dir.test()
+
 t = time.time()
 while time.time() < t + 5:
     motor.setSpeed(50)
