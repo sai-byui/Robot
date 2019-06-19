6cf1ead0449245e0a6911b05058960e1813db451
axle12693 <axle12693@gmail.com>
2019-06-18 18:40:53 -0600

    commit

---
+++ Sunfounder_Smart_Video_Car_Kit_for_RaspberryPi-master/server/cameraCode.py
@@ -0,0 +1,13 @@
+from picamera import PiCamera
+from time import sleep, time
+import datetime
+
+camera = PiCamera()
+camera.vflip = True
+camera.hflip = True
+camera.start_preview()
+
+for i in range(100):
+	sleep(.25)
+	#camera.capture('home/pi/Pictures/image%s.jpg' % datetime.datetime.now())
+camera.stop_preview()
